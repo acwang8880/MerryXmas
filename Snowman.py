ea2c5435251588e9a1aafcdd10d3ba048f214360
Alex W <acwang@berkeley.edu>
2015-12-11 18:33:30 -0800

    initial commit

---
+++ Snowman.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+
+from time import sleep
+import sys
+
+def print_slowly(text):
+    for c in text:
+        print(c, end='')
+        sys.stdout.flush()
+        sleep(0.01)
+
+for i in range(2):
+
+    print_slowly(
+"            .-~~\                          \n"
+"           /     \ _                       \n"
+"           ~x   .-~_)_                     \n"
+"             ~x-.-~   ~-.                  \n"
+"         _   ( /         \   _             \n"
+"         ||   T  o  o     Y  ||            \n"
+"       ==:l   l   <       !  I;==          \n"
+"          \\   \  .__/   /  //             \n"
+"           \\ ,r--,___.--r.//              \n"
+"            }^ \.( )   _.-//.              \n"
+"           /    }~Xi--~  //  \             \n"
+"          Y    Y I\ \    -    Y            \n"
+"          |    | |o\ \        |            \n"
+"          |    l_l  Y T       |            \n"
+"          l      =o l_j       !            \n"
+"           \                 /             \n"
+"    ___,.---^.    o        .^---.._____    \n"
+"~~~          -           ~            ~~~")
+
+    print_slowly(
+    "    _\/_                                    \n"
+    "     /\                                     \n"
+    "    .  .    MERRY CHRISTMAS !!!        \n"
+    "   .   V.                                   \n"
+    "  .V     .   \n"
+    "   '.  ..o          I should be more productive\n"
+    " o .  '  .                                  \n"
+    "  .    V  .             but Im so bored\n"
+    " .  V      .                                \n"
+    "o V.     .'o                I dont want to do hw\n"
+    "  ' '''''..                                    \n"
+    " '      V  .                                    \n"
+    "'     V     '                                   \n"
+    "o'V        .'                                   \n"
+    "   '''_'''' o                                   \n"
+    "   __|_|__                                      \n"
+    "   \     /                                      \n"
+    "    \___/                                       \n"
+    "  ~The End~")
